af16fc033a7542de81c1e7a0309905146ed6dab6
cyberar <cybercodergirl@gmail.com>
2026-03-27 20:45:22 +0100

    feat: deploy iac

--- 01-provider.tf
+++ 01-provider.tf
@@ -5,10 +5,10 @@
 
 terraform {
   required_providers {
-    namecheap = {
-      source = "namecheap/namecheap"
-      version = "2.2.0"
-    }
+    # namecheap = {
+    #   source = "namecheap/namecheap"
+    #   version = "2.2.0"
+    # }
     aws = {
       source = "hashicorp/aws"
       version = "6.0.0-beta2"
@@ -16,13 +16,13 @@ terraform {
   }
 }
 
-provider "namecheap" {
-  user_name = var.namecheap_username
-  api_user = var.namecheap_api_user
-  api_key = var.namecheap_api_key
-  client_ip = var.namecheap_client_ip
-  use_sandbox = false
-}
+# provider "namecheap" {
+#   user_name = var.namecheap_username
+#   api_user = var.namecheap_api_user
+#   api_key = var.namecheap_api_key
+#   client_ip = var.namecheap_client_ip
+#   use_sandbox = false
+# }
 provider "aws" {
   region = "us-east-1"
 }
